1e34bbd0a9df728f2582828c5ad19615101db03b
Nikita <nikitastelmashenko@gmail.com>
2018-08-13 19:21:25 +0300

    fix remove with filter in postReducer

--- source/bus/posts/reducer.js
+++ source/bus/posts/reducer.js
@@ -13,7 +13,7 @@ export const postsReducer = (state = initialState, action) => {
         case types.CLEAR_POSTS:
             return state.clear();
         case types.REMOVE_POST:
-            return state.shift(fromJS(action.payload));
+            return state.filter((post) => post.get('id') !== action.payload);
         default:
             return state;
     }
